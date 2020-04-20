a196af62e4d04cbee1ef8948d0d9fc5bea8ae08d
Phi Long <54442180+nphi-long@users.noreply.github.com>
2020-04-20 22:45:30 +0700

    20-04-2020

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './backend/admin/admin.component';
+import { DashboardComponent } from './backend/dashboard/dashboard.component';
+import { ProductListComponent } from './backend/product-list/product-list.component';
+import { HomeComponent } from './frontend/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    DashboardComponent,
+    ProductListComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
